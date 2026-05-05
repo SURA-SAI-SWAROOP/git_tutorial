2b2ee4287c3ac0cc00b0ba01d2a27957843600f9
SURA-SAI-SWAROOP <saiswaroop635@gmail.com>
2026-05-05 11:34:34 +0530

    updated the test bench

---
+++ tb_and.v
@@ -0,0 +1,22 @@
+module tb_and;
+  reg a,b;
+  wire y,ybar;
+
+  and_g dut(.a(a),.b(b),.y(y),.ybar(ybar));
+
+  initial begin
+    a=0;b=0;
+    #10;
+    a=0;b=1;
+    #10;
+    a=1;b=0;
+    #10;
+    a=1;b=1;
+    #10;
+    $finish;
+  end
+
+  initial begin
+    $monitor("a=%0b,b=%0b,y=%0b,ybar=%0b",a,b,y,ybar);
+  end
+endmodule
